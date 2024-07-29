4c2901e0355e672b616bc20b3bad177f59d18966
RootOfYggdrasill <simonedel94@gmail.com>
2024-07-29 12:04:25 -0400

    added mariadb nginx working

--- Makefile
+++ Makefile
@@ -3,21 +3,27 @@ name = inception
 
 all:
 	@printf "Launch configuration ${name}\n"
+#	if [ ! -d "./srcs/requirements/mariadb/conf/db-volume" ]; then \
+#		mkdir ./srcs/requirements/mariadb/conf/db-volume \
+#	fi
 	@docker-compose -f srcs/docker-compose.yml up -d
 
 build:
 	@printf "Building configuration ${name}...\n"
+	if [ ! -d "./srcs/requirements/mariadb/conf/db-volume" ]; then \
+		mkdir ./srcs/requirements/mariadb/conf/db-volume \
+	fi
 	@docker-compose -f srcs/docker-compose.yml up -d --build
 
 # Target to stop the configuration
 down:
 	@printf "Stopping configuration ${name}...\n"
-	@docker-compose -f ./docker-compose.yml down
+	@docker-compose -f ./srcs/docker-compose.yml down
 
 # Target to rebuild the configuration
 re:	down
 	@printf "Rebuild configuration ${name}...\n"
-	@docker-compose -f ./docker-compose.yml up -d --build
+	@docker-compose -f ./srcs/docker-compose.yml up -d --build
 
 # Target to clean up unused Docker images
 clean: down
@@ -31,6 +37,9 @@ fclean:
 	@docker system prune --all --force --volumes
 	@docker network prune --force
 	@docker volume prune --force
+
+dockersp:
+	docker-compose -f ./srcs/docker-compose.yml $(ARGS)
 
 # Declare phony targets to avoid conflicts with file names
 .PHONY	: all build down re clean fclean
---
+++ srcs/requirements/mariadb/tools/db_initialize.sh
@@ -0,0 +1,56 @@
+#!bin/sh
+if [ ! -d /var/log/mysql ]; then
+
+    mkdir -p /var/log/mysql
+
+fi
+
+if [ ! -d /run/mysqld ]; then
+
+    mkdir -p /run/mysqld
+
+fi
+
+
+# chown -R mysql:mysql /var/log/mysql
+# chown -R mysql:mysql /run/mysqld
+# chown -R mysql:mysql /var/lib/mysql
+# chown -R 777 /var/log/mysql
+# chown -R 777 /var/lib/mysql
+
+
+#	First check if mysql exist
+if [ ! -d "/var/lib/mysql" ]; then
+		#change owner with flag recursive
+        #chown -R mysql:mysql /var/lib/mysql
+
+        # init database
+        mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm 
+
+        tfile=`mktemp`
+        if [ ! -f "$tfile" ]; then
+                return 1
+        fi
+fi
+
+
+
+if [ ! -d "/var/lib/mysql/wordpress" ]; then
+
+        cat << EOF > /tmp/create_db.sql
+USE mysql;
+FLUSH PRIVILEGES;
+DELETE FROM     mysql.user WHERE User='';
+DROP DATABASE test;
+DELETE FROM mysql.db WHERE Db='test';
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT}';
+CREATE DATABASE ${DB_NAME} CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE USER '${DB_USER}'@'%' IDENTIFIED by '${DB_PASS}';
+GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';
+FLUSH PRIVILEGES;
+EOF
+        # run init.sql
+        /usr/bin/mysqld --user=mysql --bootstrap < /tmp/create_db.sql
+        rm -f /tmp/create_db.sql
+fi
